cf58b8eab6046e97144107949e02cfabe41a6124
Thiago Allue <thiago.allue@hotmail.com.com>
2023-05-04 13:25:15 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [resultsPerPage, setResultsPerPage] = useState(10);
+  const [searchHistory, setSearchHistory] = useState([]);
+
+  const handleSearchQuery = () => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa&limit=100`)
+      .then(response => response.json())
+      .then(data => setSearchResults(data.data));
+  };
+
+  const handleLoadMore = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handleClearResults = () => {
+    setSearchQuery('');
+    setSearchResults([]);
+  };
+
+  const handleAddToSearchHistory = (query) => {
+    setSearchHistory([...searchHistory, query]);
+    fetch('MY_BACKEND_API_URL', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    });
+  };
+
+  const handleClearSearchHistory = () => {
+    setSearchHistory([]);
+    fetch('MY_BACKEND_API_URL', {
+      method: 'DELETE'
+    });
+  };
+
+  useEffect(() => {
+    fetch('MY_BACKEND_API_URL')
+      .then(response => response.json())
+      .then(data => setSearchHistory(data));
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery !== '') {
+      handleSearchQuery();
+    }
+  }, [searchQuery]);
+
+  const indexOfLastResult = currentPage * resultsPerPage;
+  const indexOfFirstResult = indexOfLastResult - resultsPerPage;
+  const currentResults = searchResults.slice(indexOfFirstResult, indexOfLastResult);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Search here your Giphy</p>
+        <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+        <button onClick={handleSearchQuery}>search</button>
+        <button onClick={handleClearResults}>clear results</button>
+        <br />
+        {currentResults.map(result => (
+          <img key={result.id} src={result.images.fixed_height.url} alt={result.title} />
+        ))}
+        <br />
+        <button onClick={handleLoadMore}>load more</button>
+        <br />
+        <ul>
+          {searchHistory.map(query => (
+            <li key={query}>{query}</li>
+          ))}
+        </ul>
+        <button onClick={handleClearSearchHistory}>clear search history</button>
       </header>
     </div>
   );
